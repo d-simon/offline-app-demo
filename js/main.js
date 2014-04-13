42e2042c5ae2908e211668ec39ed0c356178b332
David Simon <me@davidsimon.ch>
2014-04-13 14:07:09 +0200

    [Setup] Basic App

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+(function () {
+    'use strict';
 
+    angular.module('offlineApp', [])
+    .controller('AppCtrl', ['$scope', '$window',  function ($scope, $window) {
+        $scope.online = navigator.onLine || false;
+        $window.addEventListener("offline", function() {
+            $scope.$apply(function() {
+                $scope.online = false;
+            });
+        }, false);
+        $window.addEventListener("online", function() {
+            $scope.$apply(function() {
+                $scope.online = true;
+            });
+        }, false);
+    }]);
+
+}());
